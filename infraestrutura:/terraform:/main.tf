3b4d94369a0fbf251fb792d6fb092b9aeecde4c0
Luana Silva Gonçalves <96505397+luanagoncalves28@users.noreply.github.com>
2024-12-30 11:21:38 -0300

    Update main.tf

--- infraestrutura:/terraform:/main.tf
+++ infraestrutura:/terraform:/main.tf
@@ -1,1 +1,36 @@
+resource "google_compute_network" "vpc_network" {
+  name                    = var.network_name
+  auto_create_subnetworks = false
+  project                 = var.project_id
+}
 
+resource "google_compute_subnetwork" "vpc_subnetwork" {
+  count         = length(var.subnets)
+  name          = var.subnets[count.index].subnet_name
+  ip_cidr_range = var.subnets[count.index].subnet_ip
+  region        = var.subnets[count.index].subnet_region
+  network       = google_compute_network.vpc_network.id
+  project       = var.project_id
+}
+
+resource "google_compute_firewall" "allow_internal" {
+  name    = "allow-internal"
+  network = google_compute_network.vpc_network.name
+  project = var.project_id
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+
+  source_ranges = [for subnet in var.subnets : subnet.subnet_ip]
+}

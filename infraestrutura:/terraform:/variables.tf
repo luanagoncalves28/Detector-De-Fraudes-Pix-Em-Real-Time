85a44b247ca4581fa7aa1726b0d3955c848c2d85
Luana Silva Gonçalves <96505397+luanagoncalves28@users.noreply.github.com>
2024-12-30 11:26:32 -0300

    Update variables.tf

--- infraestrutura:/terraform:/variables.tf
+++ infraestrutura:/terraform:/variables.tf
@@ -1,1 +1,10 @@
+variable "project_id" {
+  description = "The ID of the GCP project"
+  type        = string
+}
 
+variable "region" {
+  description = "The GCP region to deploy resources"
+  type        = string
+  default     = "us-central1"
+}

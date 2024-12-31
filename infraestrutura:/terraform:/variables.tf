1bf0952a2a7a7e1add207187fc930540e2c6be36
Luana Silva Gonçalves <96505397+luanagoncalves28@users.noreply.github.com>
2024-12-31 19:42:26 -0300

    Update variables.tf

--- infraestrutura:/terraform:/variables.tf
+++ infraestrutura:/terraform:/variables.tf
@@ -1,10 +1,40 @@
 variable "project_id" {
-  description = "The ID of the GCP project"
+  description = "O ID do projeto GCP"
   type        = string
 }
 
 variable "region" {
-  description = "The GCP region to deploy resources"
+  description = "A região GCP para implantar os recursos"
   type        = string
-  default     = "us-central1"
+  default     = "us-central1" 
+}
+
+variable "zone" {
+  description = "A zona GCP para implantar os recursos"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "cluster_name" {
+  description = "O nome do cluster GKE"
+  type        = string
+  default     = "fraud-detection-cluster"
+}
+
+variable "machine_type" {
+  description = "O tipo de máquina para os nós do cluster"
+  type        = string
+  default     = "n1-standard-4"
+}
+
+variable "min_node_count" {
+  description = "O número mínimo de nós no cluster"
+  type        = number
+  default     = 1
+}
+
+variable "max_node_count" {
+  description = "O número máximo de nós no cluster"
+  type        = number
+  default     = 5
 }
